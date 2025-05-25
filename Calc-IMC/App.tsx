0ddfa1d8ac6262201e813eb28b3a1c2b8ab2b7aa
GioEvaristo <giiofonseca08@gmail.com>
2025-05-25 17:51:41 -0300

    tentativa2

--- Calc-IMC/App.tsx
+++ Calc-IMC/App.tsx
@@ -6,44 +6,46 @@ export default function App() {
   const logo = require('./assets/imc_logo.png');
   const background = require('./assets/icon.png');
 
-  const[mensagem, setMensagem] = useState('');
-  const[imc, setIMC] = useState('');
+  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState('');
+  const [resultado, setResultado] = useState('');
+  const [imc, setIMC] = useState(0);
   
 
   function calculaIMC(altura: number, quilos: number){
-
+    let resultado = '';
 
     altura = altura / 100;
-    imc = quilos / (altura * altura);
-    return imc;
+    const imc = quilos / (altura * altura);
 
-    console.log(imc);
+    if (imc < 17) {
+      resultado = ('Muito abaixo do peso');
+    } else if (imc > 17 && imc <= 18.49) {
+      resultado = ('Abaixo do peso');
+    } else if (imc >= 18.5 && imc <= 24.99) {
+      resultado = ('Peso normal');
+    } else if (imc >= 25 && imc <= 29.99) {
+      resultado = ('Acima do peso');
+    } else if (imc >= 30 && imc <= 34.99) {
+      resultado = ('Obesidade I');
+    } else {
+      resultado = ('Obesidade II');
+    }
+
+    setIMC(imc);
+    setResultado(resultado);
+    return({setIMC});
   }
-
-  function verificarIMC(imc) {
-    let mensagem = '';
-
-  if (imc < 17) {
-    mensagem = ('Muito abaixo do peso');
-  } else if (imc > 17 && imc <= 18.49) {
-    mensagem = ('Abaixo do peso');
-  } else if (imc >= 18.5 && imc <= 24.99) {
-    mensagem = ('Peso normal');
-  } else if (imc >= 25 && imc <= 29.99) {
-    mensagem = ('Acima do peso');
-  } else if (imc >= 30 && imc <= 34.99) {
-    mensagem = ('Obesidade I');
-  } else {
-    mensagem = ('Obesidade II');
-  }
-}
 
   return (
     <View style={styles.container}>
-       <Image source={logo} style={styles.logo}></Image>
+      <Image source={logo} style={styles.logo}></Image>
       <Text style={styles.text}>Calculadora de IMC</Text>
-      <Text style={styles.subtext}>Altura: <TextInput style={styles.input} placeholder='Digite sua altura em cm' keyboardType='decimal-pad'></TextInput></Text>
-      <Text style={styles.subtext}>Peso: <TextInput style={styles.input} placeholder='Digite seu peso em quilos' keyboardType='decimal-pad'></TextInput></Text>
+      <Text style={styles.subtext}>Altura: <TextInput style={styles.input} onChangeText={(valor) => setAltura(valor)} value={altura} placeholder='Digite sua altura em cm' keyboardType='decimal-pad'></TextInput></Text>
+      <Text style={styles.subtext}>Peso: <TextInput style={styles.input} onChangeText={(valor) => setPeso(valor)} value={peso} placeholder='Digite seu peso em quilos' keyboardType='decimal-pad'></TextInput></Text>
+      <Button onPress={() => calculaIMC(parseFloat(altura), parseFloat(peso))} title='Calcular'></Button>
+      <Text style={styles.subtext}>IMC = {imc}</Text>
+      <Text style={styles.subtext}>{resultado}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -63,12 +65,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   subtext: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
     color: '#ffe45e',
-  },
-  input: {
-    fontSize: 20,
   },
   logo: {
     width: 80,
@@ -76,18 +75,15 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
-  img: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  sorteado:{
-    fontSize: 60,
+  input:{
+    fontSize: 20,
     fontWeight: 'bold',
     color: 'white',
     borderWidth: 3,
     borderColor: '#ff629d',
-    borderRadius: 200,
-    padding: 12,
+    borderRadius: 10,
+    padding: 10,
     backgroundColor: '#5aa9e6',
-  }
+    margin: 3,
+  },
 });
